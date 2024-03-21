b592699d69a7ab4017460b941f53b38ce233bc3f
Harsheet <harsheetgangwar@gmail.com>
2024-03-22 03:01:27 +0530

    writing new user routes and contoller for watch history

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,16 @@
 import { Router } from "express";
-import registerUser, { loginUser, logoutUser, refreshAccessToken } from "../controllers/user.controller.js";
+import registerUser, {
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getWatchHistory,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+} from "../controllers/user.controller.js";
 import { upload } from "../midlewares/multer.middleware.js";
 import { verifyJWT } from "../midlewares/auth.middleware.js";
 
@@ -11,21 +22,32 @@ router.route("/register").post(
     {
       name: "avatar",
       maxCount: 1,
-    }, 
+    },
     {
       name: "coverimage",
       maxCount: 1,
-      }
-      //two objects for images and avatar in 1 file
+    },
+    //two objects for images and avatar in 1 file
   ]),
   registerUser
 );
 
-
-router.route("/login").post( loginUser )
+router.route("/login").post(loginUser);
 
 //secured routes
-router.route("/logout").post(verifyJWT, logoutUser)
+router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails); //here we use patch so that badme sab kuch update na hojae
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
 
+router
+  .route("/cover-image")
+  .patch(verifyJWT, upload.single("coverimage"), updateUserCoverImage);
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile); //for params we use different syntax
+router.route("/history").get(verifyJWT, getWatchHistory);
 export default router;
